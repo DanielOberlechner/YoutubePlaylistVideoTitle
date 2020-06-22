4338ea5f9091bcd135c5ba3bfa059900c00de925
Daniel Oberlechner <danieloberlechner@hotmail.de>
2020-06-22 18:50:30 +0200

    backup before making the thumbnails downloadable

--- index.py
+++ index.py
@@ -4,10 +4,11 @@
 import requests
 import json
 import datetime
+import shutil
 
 currentDate = datetime.datetime.now().strftime("%d %B %Y.txt")
-apiKey = ""
-playlistId = "PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj" # random 200 pop songs on youtube
+apiKey = "AIzaSyCd3VTnZ6hwGHKpUz52dsV8lrthFU3pUKU"
+playlistId = "PLuP1wAti74xC6WBir_ZnRQ0lMMNH9U0Hc" # random 200 pop songs on youtube
 
 file = open(currentDate, "w")
 part = "snippet"
@@ -27,24 +28,36 @@ def doRequest(pageToken):
 
     result = requests.get(url)
     requestJSON = json.loads(result.content)
-
+    token = ""
     allVideoCounter = requestJSON["pageInfo"]["totalResults"]
-
-    for i in range(50):
-        actualVideo = requestJSON["items"][i]["snippet"]["position"]
-        actualTitle = requestJSON["items"][i]["snippet"]["title"]
-
-        videoData = str(actualVideo) + " - " + actualTitle + "\n"
-
-        print(videoData)
-        file.write(videoData)
-
-        if actualVideo == allVideoCounter - 1:
-            print("All Videos are displayed... Exit program")
-            exit(0)
-
     if "nextPageToken" in requestJSON:
-        doRequest(requestJSON["nextPageToken"])
+        token = requestJSON["nextPageToken"]
+    print(requestJSON)
+    for i in range(50):
+        try:
+            actualVideo = requestJSON["items"][i]["snippet"]["position"]
+            actualTitle = requestJSON["items"][i]["snippet"]["title"]
+            actualImage = requestJSON["items"][i]["snippet"]["thumbnails"]["default"]["url"]
+
+            image = requests.get(actualImage, stream=True)
+            with open(actualVideo + ".jpg", "wb") as out_file:
+                shutil.copyfileobj(image.raw, out_file)
+            del actualImage
+
+            videoData = str(actualVideo) + " - " + actualTitle + "\n"
+
+            print(videoData)
+            file.write(videoData)
+
+            if actualVideo == allVideoCounter - 1:
+                print("All Videos successfully displayed... Exit program")
+                break
+                exit(0)
+        except:
+            print('A error has occured! continue with next video ...')
+            continue
+    if token != "":
+        doRequest(token)
 
 
 doRequest("")
