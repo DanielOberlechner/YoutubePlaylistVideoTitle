091ecbd0659ef9f77f4f745ebbd3778c77d13586
Daniel Oberlechner <danieloberlechner@hotmail.de>
2021-09-30 12:59:08 +0200

    Update program to new youtube api guidlines, make extra folder for every playlist and save a thumbnail, videotitle and videochannelowner in that directory. There could be a small bug with not downloading all videos, but I have to still investigate this further ... :)

--- README.md
+++ README.md
@@ -1,42 +1,58 @@
-# YoutubePlaylistVideoTitle - For Music Playlists
-This Program copy's all video titles of a youtube playlist. 
+# YoutubePlaylistVideoTitle
+This Program is able to copy all video titles of multiple youtube playlists. 
 This is important because sometimes youtube deletes music titles or a user sets a video as private and so you can not see which video exactly is missing in your playlist.
 
-This short script saves all video names from a playlist into a file which has the current date as name into the directory in which the script is running.
+This short script saves all video names from multiple playlists into a directory and creates another with the current date.
 
-If a video from your playlist gets deleted you can use the created file to lookup which one it was and then search for the same video from a different uploader and add it again to your music playlist.
+If a video from your playlist gets deleted you can use the created files to lookup which one it was and then search for the same video from a different uploader and add it again to your music playlist.
 
 ## Requirements
 First you'll need a Google Account and then go to https://console.developers.google.com/project and create a new project.
 Then go to the API Keys section and create a new Youtube Data API v3 key. All of this is of course free.
 
 Insert this key into the index.py and search for the 'apiKey' variable which is on the top of the script.
-Then you need your playlist url which should be looking like the one below:
+Then you need your playlist url which should be looking like the one below: <br />
 https://www.youtube.com/watch?v=SlPhMPnQ58k&list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG
 
-What is important is the string after 'list=', so in this case the pure playlist ID would be the followning:
+What is important is the string after 'list=', so in this case the pure playlist ID would be the following:
 PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG
 
-This string you have to insert into the index.py and there into playlistId variable.
+Lets make a clear example: <br />
+You have two playlists: <br />
+1x Music Playlist <br />
+1x Random Playlist <br />
+
+Now go to "Playlists Examples" in the index.py file. <br />
+Create the following variables like this: <br />
+```python
+Music = ["Playlist_ID", "Music"]
+Random = ["Playlist_ID", "Random"]
+
+allPlaylists = [Music, Random]
+```
+
+Now you have chosen the directory name to be "Music" and "Random". <br />
+Now you're able to run the script! :)
 
 ### Restrictions:
-This Software needs Python3 to run.
+This Software needs Python3 to run and the Python module "requests". <br />
+You can install dependencies like this: <br /><br />
+pipenv install <br /> or else<br /> pip3 install requests
 
-If the playlist contains deleted or private videos it will abort when it reaches such a video. 
-It should throw an out of index error.
+### Good to know!
+If a video is deleted in your playlist, it will be shown as "Index Number - Deleted Video".
 
-If a video from a playlist which isn't from your account you have to save the foreign playlist into your account.
-When thats done you can now delete videos which aren't available anymore.
+If you have a public playlist for example from youtube directly or for example from a friend you first have to save the playlist into your own account or else you can't download it. <br />
 
-For questions or comments you can create a issue or reach me here:
+It would quit the script saying: <br />
+`404 Playlist was not found`
+
+For questions or comments you can create a issue or reach me here: <br />
+https://github.com/DanielOberlechner/YoutubePlaylistVideoTitle/issues <br />
 https://twitter.com/danielthehok
 
 Greets by 
-Daniel Oberlechner
+Daniel Oberlechner! :)
 
-
-Feel free to copy, extend or write changes to this code.
-
-Below you can see an example output:
-
-<img src="Example.png">
+Below you can see an example output: <br />
+<img src="Example.png" alt="Example image">
--- index.py
+++ index.py
@@ -7,19 +7,55 @@ import shutil
 import os
 import glob
 
-currentDate = datetime.datetime.now().strftime("%d %B %Y.txt")
-apiKey = "AIzaSyCd3VTnZ6hwGHKpUz52dsV8lrthFU3pUKU"
-playlistId = "PLuP1wAti74xC6WBir_ZnRQ0lMMNH9U0Hc" # random 200 pop songs on youtube
-part = "snippet"
-textFile = open(currentDate, "w")
-directory = "thumbnails"
+from functions import slugify, getJSON
 
-def doRequest(pageToken):
+currentDate = datetime.datetime.now().strftime("%d %B %Y.txt")
+apiKey = ""
+errorOccurred = False
+
+# Playlists Examples
+Music = ["PLAYLIST_ID", "Music"]
+Random = ["PLAYLIST_ID", "Random"]
+
+
+# Add all Playlists variables to the allPlaylist Variable to download them properly
+allPlaylists = [Music, Random]
+
+part = "snippet"
+directory = "playlists"
+
+def checkForPrivacy(playlistid):
     url = (
             "https://www.googleapis.com/youtube/v3/playlistItems?key="
             + apiKey
             + "&playlistId="
-            + playlistId
+            + playlistid[0]
+            + "&part="
+            + part
+    )
+
+    json = getJSON(url)
+    global errorOccurred
+
+    if "error" and 404 in json:
+        print(f"The playlist {playlistid[1]} is set to private!")
+        print("Please change the setting to at least unlisted!")
+        errorOccurred = True
+    elif "error" in json:
+        print(f"The following problem has occured with this playlist:")
+        print(f"{json['error']['code']}")
+        print(f"{json['error']['errors'][0]['message']}")
+        print(f"{json['error']['errors'][0]['reason']}")
+        errorOccurred = True
+    else:
+        doRequest(playlistid[0])
+
+def doRequest(elListo, pageToken=""):
+    url = (
+            "https://www.googleapis.com/youtube/v3/playlistItems?key="
+            + apiKey
+            + "&playlistId="
+            + elListo
             + "&part="
             + part
             + "&maxResults=50"
@@ -27,50 +63,94 @@ def doRequest(pageToken):
             + pageToken
     )
 
-    result = requests.get(url)
-    requestJSON = json.loads(result.content)
-    token = ""
-    allVideoCounter = requestJSON["pageInfo"]["totalResults"]
-
-    if "nextPageToken" in requestJSON:
-        token = requestJSON["nextPageToken"]
+    print(url)
+    requestJSON = getJSON(url)
 
     for i in range(50):
+        videoData = ""
+
         try:
-            actualVideo = requestJSON["items"][i]["snippet"]["position"]
             actualTitle = requestJSON["items"][i]["snippet"]["title"]
-            actualImage = requestJSON["items"][i]["snippet"]["thumbnails"]["default"]["url"]
+            actualVideoPostion = requestJSON["items"][i]["snippet"]["position"]
+            generalInformation = requestJSON["pageInfo"]["totalResults"]
 
-            image = requests.get(actualImage)
-            filer = open(str(actualVideo) + ".jpg", "wb")
-            filer.write(image.content)
-            filer.close()
+            if actualTitle == "Deleted video" or actualTitle == "Private video":
+                videoData = str(actualVideoPostion) + " - " + actualTitle
+                file = open(videoData + ".jpg", "wb")
+                file.write(thumbnail.content)
+                file.close()
+                continue
 
-            videoData = str(actualVideo) + " - " + actualTitle + "\n"
-            print(videoData)
-            textFile.write(videoData)
+            actualChannelName = requestJSON["items"][i]["snippet"]["videoOwnerChannelTitle"]
 
-            if actualVideo == allVideoCounter - 1:
-                print("All Videos successfully displayed... Exit program")
+            try:
+                actualThumbnail = requestJSON["items"][i]["snippet"]["thumbnails"]["maxres"]["url"]
+            except:
+                actualThumbnail = requestJSON["items"][i]["snippet"]["thumbnails"]["high"]["url"]
+
+            if actualVideoPostion == generalInformation - 1:
+                print("All Videos got captured by this script successfully! :)")
                 break
 
-        except:
-            print("A error has occured! continue with next video ... " + videoData + " this video could have a problem")
+            videoData = str(actualVideoPostion) + " - " + actualTitle + " by " + actualChannelName
+
+            try:
+                file = open(videoData + ".jpg", "wb")
+            except OSError:
+                file = open(str(actualVideoPostion) + " - " + slugify(actualTitle) + " by " + actualChannelName + ".jpg", "wb")
+
+            # Create Image
+            thumbnail = requests.get(actualThumbnail)
+            file.write(thumbnail.content)
+            file.close()
+
+            print(videoData)
+
+        except Exception as Error:
+            print("A error has occurred! continue with next video ... " + videoData + " this video could have a problem")
+            print(Error.with_traceback())
             continue
 
-    if token != "":
-        doRequest(token)
+    try:
+        pageToken = requestJSON["nextPageToken"]
+        if pageToken != "":
+            doRequest(elListo, pageToken)
+    except:
+        print("Everything is done now!")
 
-doRequest("")
 
-textFile.close()
+def Work():
+    os.chdir("playlists")
 
-try:
-    os.mkdir(directory)
-    source = "*.jpg"
-    destination = directory
-    for elfile in glob.glob(r'*.jpg'):
-        shutil.move(elfile, destination)
+    for playlist in allPlaylists:
+        if playlist[1] not in os.listdir(os.getcwd()):
+            os.mkdir(playlist[1])
+            os.chdir(playlist[1])
+            if currentDate not in os.listdir(os.getcwd()):
+                os.mkdir(currentDate)
+                os.chdir(currentDate)
+            else:
+                os.chdir(currentDate)
+            checkForPrivacy(playlist)
+            os.chdir("../../")
+        else:
+            os.chdir(playlist[1])
+            if currentDate not in os.listdir(os.getcwd()):
+                os.mkdir(currentDate)
+                os.chdir(currentDate)
+            else:
+                os.chdir(currentDate)
+            checkForPrivacy(playlist)
+            os.chdir("../../")
 
-except:
-    print("could not create directory or move the images ...")
+    finale()
+
+def finale():
+    if errorOccurred == False:
+        print("Everything went down just fine! :) Congratulations")
+    else:
+        print("It looks like there was some sort of problem while running this script!")
+        print("Please investigate furhter on your own or create your own issue on Github:")
+        print("https://github.com/DanielOberlechner/YoutubePlaylistVideoTitle/issues")
+
+Work()
